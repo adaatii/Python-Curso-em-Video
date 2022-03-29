902bf159b570e1dd0d379ba5bd4c2254ab8d77f6
adaatii <80161447+adaatii@users.noreply.github.com>
2022-03-29 17:35:18 -0300

    Atualização Exercicio 23.

--- PythonDesafios/d023.py
+++ PythonDesafios/d023.py
@@ -1,3 +1,10 @@
 #Faça um programa que leia um número de 0 a 9999
 # e mostre na tela cada um dos dígitos separados.
 
+num = int(input('Informe um numero: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analizando o numero {}'.format(num))
+print('Unidade: {}\nDezena: {}\nCentena: {}\nMilhar: {}'.format(u, d, c, m))
