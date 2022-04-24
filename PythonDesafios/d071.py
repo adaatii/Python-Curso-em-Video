2f6fdd8f08b896e7a4a163e98aa38b9a080f8ee6
adaatii <80161447+adaatii@users.noreply.github.com>
2022-04-24 19:38:48 -0300

    Exercicio 71.

---
+++ PythonDesafios/d071.py
@@ -0,0 +1,30 @@
+# Crie um programa que simule o funcionamento de um caixa eletrônico.
+# No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro)
+# e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
+# considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+print('='*30)
+print('{:^30}'.format('BANCO CEV'))
+print('='*30)
+valor = int(input('Qual valor você quer sacar? R$ '))
+total = valor
+ced = 50
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print('Total de {} cédulas de tantos R$ {}'.format(totalced, ced))
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+print('='*30)
+print('Volte sempre ao CEV!')
