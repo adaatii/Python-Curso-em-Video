aea36373f71ae430f9f9579da071f0ce85b0d77f
adaatii <80161447+adaatii@users.noreply.github.com>
2022-04-24 18:17:44 -0300

    Exercicio 65.

--- PythonDesafios/d065.py
+++ PythonDesafios/d065.py
@@ -1,3 +1,8 @@
+# Crie um programa que leia vários números inteiros pelo teclado.
+# No final da execução, mostre a média entre todos os valores e
+# qual foi o maior e o menor valores lidos. O programa deve perguntar
+# ao usuário se ele quer ou não continuar a digitar valores.
+
 op = 'S'
 soma = qtd = media = maior = menor =0
 while op == 'S':
