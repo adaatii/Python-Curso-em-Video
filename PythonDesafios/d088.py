7f685f59a91d15e9f36ae63717ef7315aeaf095d
adaatii <80161447+adaatii@users.noreply.github.com>
2022-04-28 16:44:45 -0300

    Exercicio 88.

---
+++ PythonDesafios/d088.py
@@ -0,0 +1,37 @@
+#  Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
+#  O programa vai perguntar quantos jogos serão gerados e vai sortear
+#  6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+
+from random import randint
+from time import sleep
+lista = []
+jogos = []
+print('-='*15)
+print('          MEGA SENA             ')
+print('-='*15)
+qtd = int(input('Quanto jogos deseja sortear? '))
+tot = 0
+while tot < qtd:
+    cont = 0
+    while cont < 6:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+    # Resolução do professor:
+    '''while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break'''
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-='*3, f'SORTEANDO {qtd} JOGOS ', '-='*3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1} : {l}')
+    sleep(1)
+print('-='*3, f'BOA SORTE', '-='*3)
