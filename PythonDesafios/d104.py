851642257d30a5ed5da8a7fda6b43a6ddccf250e
adaatii <80161447+adaatii@users.noreply.github.com>
2022-05-01 22:23:02 -0300

    Exercicio 104.

--- PythonDesafios/d104.py
+++ PythonDesafios/d104.py
@@ -7,13 +7,14 @@ def leiaInt(msg):
     while True:
         num = str(input(msg))
         if num.isnumeric():
-            valor = int(n)
+            valor = int(num)
             ok = True
         else:
-            print('\033[0;31mERRO! Digite um número inteiro válido.\033]m')
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
         if ok:
             break
     return valor
 
 
 n = leiaInt('Digite um número: ')
+print(f'Voce acabou de digitar o número {n}')
