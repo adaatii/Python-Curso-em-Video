59ddd6fd24092f175f5c50041a4224150725ac61
adaatii <80161447+adaatii@users.noreply.github.com>
2022-05-02 15:27:49 -0300

    Exercicio 107.

--- PythonDesafios/d107/teste.py
+++ PythonDesafios/d107/teste.py
@@ -7,4 +7,4 @@ p = float(input('Digite o preço: '))
 print(f'A metade do {p} é R${moeda.metade(p)}')
 print(f'O dobro de {p} é R${moeda.dobro(p)}')
 print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10)}')
-print(f'Diminuindo 13%, temos R${moeda.diminuir(p, 13)}')
+
